698627cd30a1ce8d6eaf813accec398e31cd09af
Joel Kendall <joel@45press.com>
2020-01-21 17:10:39 -0500

    fixing footer

--- footer.php
+++ footer.php
@@ -7,7 +7,7 @@
 
 <div class="footer-text">
   <p id="copyright">2019 Milan Records. A division of sony music entertainment. all rights reserved. send us feedback<span>privacy policy/your privacy rights | terms and conditions | why music matters | <a href="https://45press.com" target="_blank">built by 45press</a></span></p>
-  <div id="newsletter"><a href="#">newsletter sign up</a></div>
+  <a id="newsletter" href="#">newsletter sign up</a>
 </div>
 
 </div>
